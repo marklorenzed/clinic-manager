9715ee7aca0f285483ead839b80a593a8dc4e97d
marklorenzed <marklorenzed@gmail.com>
2023-05-17 13:59:46 +0800

    layout app foundations

---
+++ src/components/Icons.ts
@@ -0,0 +1,11 @@
+import { ChevronLeft, ChevronRight, Moon, Sun, Laptop } from "lucide-react";
+
+const Icons = {
+  ChevronLeft,
+  ChevronRight,
+  Sun,
+  Moon,
+  Laptop,
+};
+
+export default Icons;
